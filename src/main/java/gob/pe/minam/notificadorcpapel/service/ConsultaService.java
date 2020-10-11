a95c90e61c8251e71573be981393a8514bce1070
Francisco Bazan <fbazamn@gmail.com>
2020-10-11 18:30:52 -0500

    cambio

--- src/main/java/gob/pe/minam/notificadorcpapel/service/ConsultaService.java
+++ src/main/java/gob/pe/minam/notificadorcpapel/service/ConsultaService.java
@@ -15,7 +15,8 @@ public class ConsultaService implements  IConsultaService{
         StaticConsultaMinam instancia = StaticConsultaMinam.getInstancia(null);
         Consulta consultas = instancia.getConsulta();
         if(consultas==null) {
-            consultas = (Consulta) jdbcTemplate.queryForObject("SELECT SQL_NOTIFICADO, SQL_RECHAZADO, SQL_ACTUALIZAR, SQL_BUSCAR_CIUDADANO FROM CP_TBL_CONSULTA_JOB  WHERE ID_CONSULTA = 1", new BeanPropertyRowMapper(Consulta.class));
+            consultas = (Consulta) jdbcTemplate.queryForObject("SELECT SQL_NOTIFICADO, SQL_RECHAZADO, SQL_ACTUALIZAR, SQL_BUSCAR_CIUDADANO" +
+                    " FROM CP_TBL_CONSULTA_JOB  WHERE ID_CONSULTA = 1", new BeanPropertyRowMapper(Consulta.class));
             instancia.setConsulta(consultas);
         }
        return consultas;
